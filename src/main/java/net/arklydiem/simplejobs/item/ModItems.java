adf8d24fb1a21c9f44fec9ed90907bbc9fa01647
Arthur Gomez <arthur.gomez.alarcon@gmail.com>
2023-09-25 08:33:09 +0200

    0.0.0
    Just starting the project

---
+++ src/main/java/net/arklydiem/simplejobs/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.arklydiem.simplejobs.item;
+
+import net.arklydiem.simplejobs.SimpleJobs;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, SimpleJobs.MOD_ID);
+
+    public static final RegistryObject<Item> ZIRCON = ITEMS.register("zircon",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SIMPLEJOBS_TAB)));
+
+    public static void register(IEventBus eventbus) {
+        ITEMS.register(eventbus);
+    }
+
+}
